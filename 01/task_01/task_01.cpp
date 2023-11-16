e7622b7c8cc331c403cd8cb0d6c9ceff38ece6c1
AlRezepov <alexrezepovjr@mail.ru>
2023-11-17 00:40:31 +0100

    Add Homework_01

---
+++ 01/task_01/task_01.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+void client(int& count, int& count_max) {
+    while (count <= count_max)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        count++;
+        std::cout << "Client: " << count << std::endl;
+    }
+}
+
+void teller(int& count) {
+    while (count >= 0)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        count--;
+        std::cout << "Teller: " << count << std::endl;
+    }
+}
+
+int main()
+{
+    int count = 0;
+    int count_max = 16;
+    
+    std::thread t1(client, std::ref(count), std::ref(count_max));
+    std::thread t2(teller, std::ref(count));
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
---
+++ 01/task_02/task_02.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <chrono>
+
+// Функция для генерации случайных чисел в диапазоне [min, max]
+int getRandomNumber(int min, int max) {
+	return min + rand() % (max - min + 1);
+}
+
+// Функция для для складывания двух векторов
+std::vector<int> vector_sum(int threadID, std::vector<int>& a, std::vector<int>& b, int size) {
+	std::vector<int> c(size);
+	for (int i = 0; i < size; ++i)
+	{
+		c[i] = a[i] + b[i];
+	}
+	return c;
+}
+
+// Функция для заполнения вектора
+void full_vector(std::vector<int>& vector, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		vector.push_back(getRandomNumber(0, 100));
+	}
+}
+
+int main()
+{
+	std::vector<std::thread> t;
+	
+	// Вывод количества доступных аппаратных ядер
+	int num_cores = std::thread::hardware_concurrency();
+	std::cout << "Number of hardware cores: " << num_cores << std::endl;
+	std::cout << std::endl;
+
+	// Установка размеров векторов
+	std::vector<int> vector_sizes = { 1000, 10000, 100000, 1000000 };
+
+	// Установка количества потоков
+	std::vector<int> numThreads = { 2, 4, 8, 16 };
+
+	// Создание векторов
+	std::vector<int> a;
+	std::vector<int> b;
+
+	for (int i = 0; i < vector_sizes.size(); i++)
+	{
+		std::cout << "\t" << vector_sizes[i] << "\t";
+	}
+
+	std::cout << std::endl;
+
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << numThreads[i] << ": ";
+
+		for (int j = 0; j < 4; j++)
+		{
+			// Заполнение первого вектора
+			full_vector(a, vector_sizes[j]);
+
+			// Заполнение второго вектора
+			full_vector(b, vector_sizes[j]);
+
+			auto start = std::chrono::steady_clock::now();
+			t.push_back(std::thread(vector_sum, numThreads[i], std::ref(a), std::ref(b), vector_sizes[j]));
+			auto end = std::chrono::steady_clock::now();
+			std::chrono::duration<double> elapsed_seconds = end - start;
+
+			std::cout << "\t" << elapsed_seconds.count();
+
+		}
+
+		for (auto& i : t)
+		{
+			i.join();
+		}
+
+		t.clear();
+		std::cout << std::endl;
+	}
+
+}
