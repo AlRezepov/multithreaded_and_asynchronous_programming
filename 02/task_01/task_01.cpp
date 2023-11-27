6a20b160e71812f314ec573afbffa1bee7b6720b
AlRezepov <alexrezepovjr@mail.ru>
2023-11-27 22:05:50 +0100

    Add Homework_02

---
+++ 02/task_01/task_01.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <condition_variable>
+
+void client(std::atomic<int>& count, int& count_max) {
+    while (count.load(std::memory_order_relaxed) <= count_max)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        count.fetch_add(1, std::memory_order_relaxed);
+        std::cout << "Client: " << count << std::endl;
+    }
+}
+
+void teller(std::atomic<int>& count) {
+
+    while (count.load(std::memory_order_relaxed) >= 0)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        count.fetch_sub(1, std::memory_order_relaxed);
+        std::cout << "Teller: " << count << std::endl;
+    }
+
+}
+
+int main() {
+
+    std::atomic<int> count = 0;
+    int count_max = 16;
+
+    std::thread t1(client, std::ref(count), std::ref(count_max));
+    std::thread t2(teller, std::ref(count));
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
---
+++ 02/task_02/task_02.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <chrono>
+
+// Функция для для складывания двух векторов
+void progress_bar(int threadID){
+
+    std::cout << std::this_thread::get_id() << "\t";
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << "|";
+    }
+}
+
+int main()
+{
+    std::vector<std::thread> t;
+    int numThreads = 5;
+
+    std::cout << "#\t" << "id\t" << "Progress Bar\t" << "Time" << std::endl;
+
+    for (int i = 0; i < numThreads; i++)
+    {
+
+        std::cout << i << ": \t";
+
+        auto start = std::chrono::steady_clock::now();
+        t.push_back(std::thread(progress_bar, i));
+        auto end = std::chrono::steady_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end - start;
+        std::cout << "\t" << elapsed_seconds.count() << std::endl;
+    }
+
+    for (auto& i : t)
+    {
+        i.join();
+    }
+
+    t.clear();
+    std::cout << std::endl;
+}
