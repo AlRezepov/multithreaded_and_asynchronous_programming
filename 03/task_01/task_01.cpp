c557da3d3eb20fb5e193c62a1fd81360e393acb1
AlRezepov <alexrezepovjr@mail.ru>
2023-12-08 23:14:55 +0100

    Add Homework_03

---
+++ 03/task_01/task_01.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <future>
+
+// Асинхронная функция для поиска минимального элемента
+template <typename T>
+std::future<T> min_element(std::vector<T>& vec, std::size_t start, std::size_t end) {
+    std::promise<T> result_promise;
+    std::future<T> result_future = result_promise.get_future();
+
+    std::async([&]() {
+        auto min_it = std::min_element(vec.begin() + start, vec.begin() + end);
+        result_promise.set_value(*min_it);
+        });
+
+    return result_future;
+}
+
+// Сортировка выбором
+template <typename T>
+std::vector<T> selection_sort(std::vector<T> vec) {
+    std::size_t n = vec.size();
+
+    for (std::size_t i = 0; i < n - 1; ++i) {
+        
+        auto future_min = min_element(vec, i, n);
+        auto min_val = future_min.get();
+
+        // Находим индекс минимального элемента
+        auto min_it = std::find(vec.begin() + i, vec.end(), min_val);
+
+        // Обмениваем местами текущий элемент и минимальный элемент
+        std::iter_swap(vec.begin() + i, min_it);
+    }
+
+    return vec;
+}
+
+int main() {
+    std::vector<int> arr = { 150, 1, 40, 22, 5 };
+
+    // Вызываем сортировку выбором
+    auto sorted_future = std::async(std::launch::async, selection_sort<int>, arr);
+    auto sorted = sorted_future.get();
+
+    // Выводим результат
+    std::cout << "Sorted array: ";
+    for (auto& i : sorted) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ 03/task_02/task_02.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <future>
+
+template <typename It, typename Function>
+void parallel_for_each(It begin, It end, Function&& func, std::size_t threshold) {
+    std::size_t size = std::distance(begin, end);
+
+    if (size <= threshold) {
+        // Если размер диапазона меньше или равен пороговому значению, обрабатываем стандартным for_each
+        std::for_each(begin, end, func);
+    }
+    else {
+        It mid = begin + size / 2;
+
+        auto first_half = std::async(parallel_for_each<It, Function>, begin, mid, func, threshold);
+        auto second_half = std::async(parallel_for_each<It, Function>, mid, end, func, threshold);
+
+        // Ожидаем завершения задач
+        first_half.get();
+        second_half.get();
+    }
+}
+
+int main() {
+    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    // Устанавливаем пороговое значение
+    std::size_t threshold = 2;
+
+    // Пример использования параллельного for_each, где каждый элемент вектора умножается на 2
+    parallel_for_each(numbers.begin(), numbers.end(), [](int& num) {
+        num *= 2;
+        }, threshold);
+
+    std::cout << "Modified array: ";
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
